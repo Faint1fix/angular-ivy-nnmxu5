0faf453d2133f6390ed5b8d4bc598061f9cd75d2
Faint1fix <faint1fix@gmail.com>
2021-11-22 13:37:22 +0200

    1 file modified

--- src/app/components/games-list/games-list.component.ts
+++ src/app/components/games-list/games-list.component.ts
@@ -1,25 +1,27 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 
-import { Game } from 'src/app/game';
+import { Game } from '../../game';
 import { GamesService } from './../../services/games.service';
 
 @Component({
   selector: 'app-games-list',
   templateUrl: './games-list.component.html',
-  styleUrls: ['./games-list.component.css']
+  styleUrls: ['./games-list.component.css'],
 })
 export class GamesListComponent implements OnInit, OnDestroy {
   private subscriptions: Subscription[] = [];
   gamesList: Game[] = [];
 
-  constructor(private gamesService: GamesService) { }
+  constructor(private gamesService: GamesService) {}
 
   ngOnInit(): void {
-    this.subscriptions.push(this.gamesService.getGames().subscribe(list => this.gamesList = list));
+    this.subscriptions.push(
+      this.gamesService.getGames().subscribe((list) => (this.gamesList = list))
+    );
   }
 
   ngOnDestroy(): void {
-    this.subscriptions.forEach(subscr => subscr.unsubscribe());
+    this.subscriptions.forEach((subscr) => subscr.unsubscribe());
   }
 }
